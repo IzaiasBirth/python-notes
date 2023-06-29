4f3fea3f97e09a66ef22c9660bf34b1e7530fcaa
IzaiasBirth <izaiasenelly@gmail.com>
2023-06-29 11:05:29 -0300

    Using the 'idade' data

--- fatiamento.py
+++ fatiamento.py
@@ -3,10 +3,11 @@ print('\n') #Imprime espaços antes do título
 print('     INFORMAÇÕES PESSOAIS')
 print('='*35) #Imprime o sinal de igualdade 35 vezes
 nome = input('Digite seu nome: ')
-idade = input('Digite a sua idade: ')
+idade = int(input('Digite a sua idade: '))
 if idade and nome: #Condiciona a execução do programa a inclusão de duas entradas de dados
     print(f'Seu nome é: {nome}')
-    print(f'Seu nome invertido é: {nome[::-1]}') #Fatiamento da variável 'nome' do início ao fim, passando de um em um de trás pra frente (-1)
+    print(f'Você nasceu em: {2023-idade}')
+    print(f'Seu nome invertido é: {nome[::-1]}') #Fatiamento da variável 'nome' do início ao fim, passando de um em um, de trás pra frente (-1)
     print(f'A primeira letra do seu nome é: {nome[0]}')
     print(f'A última letra do seu nome é: {nome[-1]}')
     print(f'Seu nome tem {len(nome)} letras') #A função 'len' faz a contagem da quantidade de letras na variável 'nome'
