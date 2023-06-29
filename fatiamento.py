e1dfec058d15862898047b426cf3f1efc5a221f3
IzaiasBirth <izaiasenelly@gmail.com>
2023-06-29 10:48:10 -0300

    Added some comments in the code

--- fatiamento.py
+++ fatiamento.py
@@ -6,10 +6,10 @@ nome = input('Digite seu nome: ')
 idade = input('Digite a sua idade: ')
 if idade and nome: #Condiciona a execução do programa a inclusão de duas entradas de dados
     print(f'Seu nome é: {nome}')
-    print(f'Seu nome invertido é: {nome[::-1]}')
+    print(f'Seu nome invertido é: {nome[::-1]}') #Fatiamento da variável 'nome' do início ao fim, passando de um em um de trás pra frente (-1)
     print(f'A primeira letra do seu nome é: {nome[0]}')
     print(f'A última letra do seu nome é: {nome[-1]}')
-    print(f'Seu nome tem {len(nome)} letras')
+    print(f'Seu nome tem {len(nome)} letras') #A função 'len' faz a contagem da quantidade de letras na variável 'nome'
     if ' ' in nome:
         print('Seu nome contém espaço')
     else:
